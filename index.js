70c1fd37e5717f35c0e3d4783a0f2a727afb74cd
Sumit Sinha <sumitsinha215@gmail.com>
2023-12-03 18:34:09 +0530

    port changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port=process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
